942f642f2e37b259bfbba74022433edb61628239
latorreandrea <latorre.andrea.93@gmail.com>
2022-02-19 23:29:36 +0000

    added products urls and customized admin panel for products and categories

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
+# customizing admin info displayed for products
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    ordering = ('name',)
+
+
 # Register models for products to keep update the catalougue from admin panel
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
