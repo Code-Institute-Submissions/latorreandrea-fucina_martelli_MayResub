d8498ddfe66827fc4d9983126e22f64d8b214885
latorreandrea <latorre.andrea.93@gmail.com>
2022-02-20 18:15:14 +0000

    added search queryes

--- products/views.py
+++ products/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Product
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products """
+    """ A view to show all products"""
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+                            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search': query,
     }
     return render(request, 'products/products.html', context)
+
 
 
 def product_detail(request, product_id):
