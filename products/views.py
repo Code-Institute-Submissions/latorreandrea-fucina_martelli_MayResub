82fe25a412ff0a6ee0817eb67f879109835feb67
latorreandrea <latorre.andrea.93@gmail.com>
2022-02-20 18:50:54 +0000

    added filtering by category

--- products/views.py
+++ products/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products"""
+    """ A view to show products / searched products /searched category"""
     products = Product.objects.all()
     query = None
+    category = None
 
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
-                            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+        if 'category' in request.GET:
+            category = request.GET['category']           
+            products = products.filter(category__name=category)
+            
+            
 
     context = {
         'products': products,
         'search': query,
+        'category_selected': category,
     }
     return render(request, 'products/products.html', context)
 
