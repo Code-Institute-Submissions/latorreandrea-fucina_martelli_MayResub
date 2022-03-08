c0bdfc63e26cb4957a662c31b7311662cf960026
latorreandrea <latorre.andrea.93@gmail.com>
2022-03-08 10:52:55 +0000

    AuthenticationError not provided an API key

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
+from django.conf import settings
 from .forms import OrderForm
+from cart.contexts import cart_contents
 
-
+import stripe 
 # Create your views here.
 def checkout(request):
+    # create payment intent 1/2
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     # preveent perople accessin manually the Url
     if not cart:
-        messages.error(request, "there's nothing in your cart!")   
+        messages.error(request, "there's nothing in your cart")   
         return redirect(reverse('products'))
+    
+    current_cart = cart_contents(request)
+    total = current_cart['total']
+
+    # create payment intent 2/2
+    stripe_total = round(total*100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
