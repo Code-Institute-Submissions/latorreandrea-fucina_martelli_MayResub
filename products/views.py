c0eced04b61f2d332ef478e16fae501ba904c6df
latorreandrea <latorre.andrea.93@gmail.com>
2022-03-21 20:00:28 +0000

    fixed bug in search queries adding else statement in def all products

--- products/views.py
+++ products/views.py
@@ -10,23 +10,30 @@ from accounts.models import Account
 
 def all_products(request):
     """ A view to show products / searched products /selected category"""
-    products = Product.objects.all()
-    #paginating (https://docs.djangoproject.com/en/4.0/topics/pagination/)
-    paginator = Paginator(products, 6) # Show 6 products per page.
-    page_number = request.GET.get('page')
-    products = paginator.get_page(page_number) 
+    products = Product.objects.all()      
 
     query = None
-    category = None
+    category = None   
 
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+            paginator = Paginator(products, 6) # Show 6 products per page.
+            page_number = request.GET.get('page')
+            products = paginator.get_page(page_number)
+            
         if 'category' in request.GET:
             category = request.GET['category']           
             products = products.filter(category__name=category)
+            paginator = Paginator(products, 6) # Show 6 products per page.
+            page_number = request.GET.get('page')
+            products = paginator.get_page(page_number)
+    else :
+        paginator = Paginator(products, 6) # Show 6 products per page.
+        page_number = request.GET.get('page')
+        products = paginator.get_page(page_number)
 
     context = {
         'products': products,
