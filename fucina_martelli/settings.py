5a56a79ae634b46b4f578674e2d441f1faa6c02e
latorreandrea <latorre.andrea.93@gmail.com>
2022-04-04 19:19:13 +0000

    modified vars for deploy

--- fucina_martelli/settings.py
+++ fucina_martelli/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#j=473zji#9h#%cm9+$ew&+egl+*6i0e0&^hf1^xivq@f1b9o_'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fucina-martelli.herokuapp.com', 'localhost']
 
