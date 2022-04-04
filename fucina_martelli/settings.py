a35e2d8cbb18b28489d7f8656d21aa075d9037d3
latorreandrea <latorre.andrea.93@gmail.com>
2022-04-04 19:02:24 +0000

    second attempt on heroku deploy

--- fucina_martelli/settings.py
+++ fucina_martelli/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-#j=473zji#9h#%cm9+$ew&+egl+*6i0e0&^hf1^xivq@f1b9o_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fucina-martelli.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'fucina_martelli.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
